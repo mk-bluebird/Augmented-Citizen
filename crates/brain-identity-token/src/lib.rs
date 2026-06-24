71c210011061542acd7d4419747fc6478eae9c5c
mk-bluebird <2bf5zk7ksg@privaterelay.appleid.com>
2026-06-23 18:15:44 -0700

    Create lib.rs

---
+++ crates/brain-identity-token/src/lib.rs
@@ -0,0 +1,9 @@
+// crates/brain-identity-token/src/lib.rs
+
+#![forbid(unsafe_code)]
+
+mod types;
+mod pain_extension;
+
+pub use types::BrainIdentityToken;
+pub use pain_extension::BrainIdentityPainExtension;
